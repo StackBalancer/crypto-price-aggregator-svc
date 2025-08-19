15be63ef489cd469e79e04626d0d28b9a8bbbe22
StackBalancer <tonko.knez@protonmail.com>
2025-08-19 18:22:00 +0200

    Use pre-created ECS task execution role

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,24 +13,8 @@ resource "aws_ecs_cluster" "crypto_service" {
 }
 
 # IAM role for ECS task execution
-resource "aws_iam_role" "ecs_task_execution_role" {
+data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Task Definition
@@ -41,7 +25,7 @@ resource "aws_ecs_task_definition" "crypto_service" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
